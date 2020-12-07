71ae82783bf4761be7d14e87d4de5abe4b2393f1
EricShi1255 <eshi20@stuy.edu>
2020-12-07 12:53:16 -0500

    constructor

---
+++ ArraySequence.java
@@ -0,0 +1,18 @@
+import java.util.NoSuchElementException;
+public class ArraySequence implements IntegerSequence{
+  int currentIndex;
+  int []data;
+
+  /*Construct the sequence by copying values from the other array into the data array*/
+  public ArraySequence(int [] other){
+    for (int item: other) {
+        data.add(item);
+    }
+  }
+  ////Postcondition: The otherseq will be reset.
+  ////This constructor will copy ALL values of the `otherseq` into the data array.
+  public ArraySequence(IntegerSequence otherseq){ 
+
+  }
+  
+}
