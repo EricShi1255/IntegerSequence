6f7e58abf4b2339d7409e2f116dbba30680baae3
EricShi1255 <eshi20@stuy.edu>
2020-12-07 22:59:11 -0500

    short test

--- ArraySequence.java
+++ ArraySequence.java
@@ -30,7 +30,6 @@ public class ArraySequence implements IntegerSequence{
     return data.length;
   }
 
-
   public boolean hasNext() {
     return currentIndex < data.length;
   }
@@ -42,6 +41,17 @@ public class ArraySequence implements IntegerSequence{
     currentIndex++;
     return curr;
   }
+  /*
+  public static void main(String[] args) {
+    int[]nums = {1,3,5,0,-1,3,9};
+    IntegerSequence as = new ArraySequence(nums);
+
+    System.out.println("ArraySequence(array):");
+    while(as.hasNext()){
+      System.out.print(as.next()+", ");
+    }
+  }
+  */
   
   
 
